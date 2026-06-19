0b5a05b6c56e8917bf964078adebcc945c055f47
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-06-19 13:48:26 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+export default function Navbar() {
+    return (
+        <>
+            <div className="nav">
+                <span className="logo">AI Dashboard</span>
+                <span className="user-name">Laiba's Space</span>
+            </div>
+
+        </>
+    )
+
+}
