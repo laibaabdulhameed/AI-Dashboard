ab795a4dd468ebf1e5a801b9e508ebfbd266f726
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-06-22 17:32:59 +0100

    feat: add sidebar navigation links with react router

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,15 @@
 import './Sidebar.css';
+import { Link } from 'react-router-dom';
 import { Home, CheckSquare, BookOpen, Target, MessageCircle, Settings } from 'lucide-react';
 export default function Sidebar({isOpen}) {
     return (
         <div className="main-div" style={{display:isOpen? "block" : "none"}}>
               <ul className='features-list'>
-                <li><Home size={18}/>Home</li>
-                <li><CheckSquare size={18}/>Tasks</li>
-                <li><BookOpen size={18}/>journals</li>
-                <li><Target size={18}/>Habit Tracker</li>
-                <li><Settings size={18}/>Settings</li>
+                <li><Link to="/home"><Home size={18}/>Home</Link></li>
+                <li><Link to="/tasks"><CheckSquare size={18}/>Tasks</Link></li>
+                <li><Link to="/journals"> <BookOpen size={18}/>journals</Link></li>
+                <li><Link to="/habit-tracker"><Target size={18}/>Habit Tracker</Link></li>
+                <li><Link to="/settings"><Settings size={18}/>Settings</Link></li>
               </ul>
         </div>
        
